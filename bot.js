5f13c3d6d5f57b5a27f5f1e5c628e5a9175dbe5d
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-01 09:50:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 
-client.login('NTQwNzc5MzcxODY3Mjc1MjY1.DzV3jw.0QzS_LfTFBdN_5TBNDF_84CVDoE'); 
+client.login('NTQwNzg0ODI5MzIzNjczNjE5.DzV9bw.uoqow8clBrSqDn17WUOS8tmuaKI'); 
 
 
 
@@ -106,24 +106,5 @@ client.on('message', message => {
 
 
 
-
-majd.on('message', function(msg) {
-if(msg.content.startsWith (prefix  + 'serverinfo')) {
- let embed = new Discord.RichEmbed()
- .setColor('RANDOM')
- .setThumbnail(msg.guild.iconURL)
- .setTitle(`Showing Details Of  **${msg.guild.name}*`)
- .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
- .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
- .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
- .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
- .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
- .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
- .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
- .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
- .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
- msg.channel.send({embed:embed});
-}
-});
 
 
