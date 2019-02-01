67077b0d9d66373f328366a3e502bc5cdd735b8d
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-01 10:34:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,5 +106,25 @@ client.on('message', message => {
 
 
 
+client.on('guildMemberAdd', member => {
 
+    const channel = member.guild.channels.find('smg', '・smg');
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+
+
+
+
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+
+  
+});
 
